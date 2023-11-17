fdf04848f3780f3948436213e87a66ff943031f1
iloozealot <iloozealot@gmail.com>
2023-11-16 20:48:59 -0600

    Add project files.

---
+++ CelsiusToFarenheightConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Please enter the temperature in degrees Farenheit to convert: ");
+double tempF = Convert.ToDouble(Console.ReadLine());
+
+double? tempC = null;
+
+tempC = (tempF - 32) / 1.8; 
+
+Console.WriteLine($"The temperature in degrees Celsius is {Math.Round((decimal)tempC)}.");
